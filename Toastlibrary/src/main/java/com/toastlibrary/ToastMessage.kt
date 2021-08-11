55e0759ffa6c4067f99511c8408d613f1c0d4c24
com <developeramit3@gmail.com>
2021-08-11 18:54:46 +0530

    first commit

---
+++ Toastlibrary/src/main/java/com/toastlibrary/ToastMessage.kt
@@ -0,0 +1,10 @@
+package com.toastlibrary
+
+import android.content.Context
+import android.widget.Toast
+
+class ToastMessage {
+    fun s(c: Context?, message: String?) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+}
