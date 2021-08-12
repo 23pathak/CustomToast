84e3a1847f4f753dc8b19a4a3714fa842b01cdd5
com <developeramit3@gmail.com>
2021-08-12 11:07:31 +0530

    second commit with custom toast

--- Toastlibrary/src/main/java/com/toastlibrary/ToastMessage.kt
+++ Toastlibrary/src/main/java/com/toastlibrary/ToastMessage.kt
@@ -1,6 +1,5 @@
 package com.toastlibrary
 
-import android.R
 import android.content.Context
 import android.view.Gravity
 import android.view.LayoutInflater
@@ -12,5 +11,16 @@ import android.widget.Toast
 class ToastMessage {
     fun s(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun toast(context: Context?,message: String?) {
+        val toast = Toast(context)
+        val view: View = LayoutInflater.from(context).inflate(R.layout.custom_toast, null)
+        val textView = view.findViewById<View>(R.id.custom_toast_text) as TextView
+        textView.text = message
+        toast.setView(view)
+        toast.setGravity(Gravity.BOTTOM or Gravity.CENTER, 0, 0)
+        toast.duration = Toast.LENGTH_LONG
+        toast.show()
     }
 }
